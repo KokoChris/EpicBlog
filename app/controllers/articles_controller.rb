38116e99163750464198c8151ad70b2a29354bc6
Chris Kokolios <kokolios1986@yahoo.gr>
2015-12-17 20:22:52 +0200

    installed bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,12 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article , only:[:edit, :update, :show, :destroy]
   def index
      @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
   def edit
-    @article = Article.find(params[:id])  
   end  
 
   def new
@@ -16,7 +14,6 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfuly created" 
       redirect_to article_path(@article)
@@ -25,7 +22,6 @@ class ArticlesController < ApplicationController
   end
   end  
   def update
-    @article = Article.find(params[:id])
       if @article.update(article_params)
         flash[:notice] = "Article was successfuly updated" 
         redirect_to article_path(@article)
@@ -35,7 +31,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
     flash[:notice] = "Article was successfuly deleted"
@@ -43,6 +38,10 @@ class ArticlesController < ApplicationController
 
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+ 
     def article_params
       params.require(:article).permit(:title, :description)
     end
