0f92481d495bf1789cef2249fd67a104e6971639
Chris Kokolios <kokolios1986@yahoo.gr>
2015-12-20 17:29:33 +0200

    Finished auth system

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout',to: 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy', as: "logout"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
